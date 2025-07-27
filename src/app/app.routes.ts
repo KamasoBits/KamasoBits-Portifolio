ccb9ede741c368496d1445b3c02ce8e07d8dbb25
KamasoBits <kitkatkamaso@gmail.com>
2025-07-27 21:12:01 +0300

    navbar creation

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { PageNotFoundComponent } from './Components/page-not-found/page-not-found.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { PackagesComponent } from './Components/packages/packages.component';
+import { ProjectsComponent } from './Components/projects/projects.component';
+import { ContactComponent } from './Components/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'about',component:AboutComponent},
+    {path:'packages',component:PackagesComponent},
+    {path:'projects',component:ProjectsComponent},
+    {path:'contact',component:ContactComponent},
+    {path:'**',component:PageNotFoundComponent}
+
+];
