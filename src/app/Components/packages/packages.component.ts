145c0ba0f74e69d3d4f4393812b40b68db29f019
KamasoBits <kitkatkamaso@gmail.com>
2025-08-04 11:06:53 +0300

    packages componet done

--- src/app/Components/packages/packages.component.ts
+++ src/app/Components/packages/packages.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-packages',
   imports: [
-    MatIconModule
+    RouterLink,
+    RouterLinkActive
 
   ],
   templateUrl: './packages.component.html',
