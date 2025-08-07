5d1501bfc71e167c19e52f70d8b4ef41cc77cb93
KamasoBits <kitkatkamaso@gmail.com>
2025-08-07 12:58:23 +0300

    done

--- src/app/Services/contacts.service.ts
+++ src/app/Services/contacts.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactsService {
 
-  constructor() { }
+  contactDetails = {
+    name: '',
+    email: '',
+    subject: '',
+    message: '',
+  }
+
+  constructor(private http: HttpClient) { }
+
+  sendContact(){
+    this.http.post('', this.contactDetails,{responseType:'text'}).subscribe({
+      next:(res: any) => alert(res),
+      error:(error: { error: string; }) => alert('Failde to Send:' +error.error)
+    })
+  }
 }
