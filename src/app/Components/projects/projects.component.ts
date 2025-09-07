81f33288351a83d989d6a83f6602cb4179f00130
KamasoBits <kitkatkamaso@gmail.com>
2025-09-07 16:38:22 +0300

    simple edits

--- src/app/Components/projects/projects.component.ts
+++ src/app/Components/projects/projects.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import {MatIconModule} from '@angular/material/icon';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+
 
 @Component({
   selector: 'app-projects',
   imports: [
     FontAwesomeModule,
-    MatIconModule
+    MatIconModule,
+    MatProgressBarModule
+   
   ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
